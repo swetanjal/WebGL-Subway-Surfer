83a3d8179a3f36581a3035a66e6e0ea61de09dd2
swetanjal <swetanjal.dutta@research.iiit.ac.in>
2019-03-08 23:35:16 +0530

    Add coin objects, music.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ main();
 // Start here
 //
 function main() {
+  //document.getElementById('backaudio').play();
   const canvas = document.querySelector('#glcanvas');
   const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
 
@@ -68,6 +69,7 @@ function main() {
   var buffer;
   var tracks = [];
   var trains = [];
+  var coins = [];
   var player;
   var inspector;
   var then = 0;
@@ -75,60 +77,74 @@ function main() {
   var textureTrack;
   var texturePlayer;
   var textureInspector;
+  var textureCoin;
   loadJSONResource('./tracks.json', function (modelErr, modelTrack) {
     loadJSONResource('./train.json', function(modelErr2, modelTrain){
       loadJSONResource('./player.json', function(modelErr3, modelPlayer){
         loadJSONResource('./inspector.json', function(modelErr4, modelInspector){
-          textureTrain = loadTexture(gl, 'train.png');
-          textureTrack = loadTexture(gl, 'tracks.png');
-          texturePlayer = loadTexture(gl, 'player.png');
-          textureInspector = loadTexture(gl, 'inspector.png');
-          /* Generating trains */
-          trains.push(initBuffersModel(gl, 1, 1, 1, 0, 2, -150, [1.0, 1.0, 0.0, 1.0],modelTrain));
-          trains[0].rotation = 180;
-          trains[0].scale = [0.25, 0.25, 0.25];
-          /************************************************************/
-          /* Generating tracks */
-          var z = -10000;
-          var cnt = 0;
-          for(var i = 0; i < 200; ++i){
-            tracks.push(initBuffersModel(gl, 0, 0, 0, 0, -50, z, [1.0, 1.0, 0.0, 1.0], modelTrack));
-            tracks[cnt].rotation = 90;
-            tracks[cnt].scale = [0.1, 0.1, 0.1];
-            z += 120;
-            cnt = cnt + 1;
-          }
-          z = -10000;
-          for(var i = 0; i < 200; ++i){
-            tracks.push(initBuffersModel(gl, 0, 0, 0, 58, -50, z, [1.0, 1.0, 0.0, 1.0], modelTrack));
-            tracks[cnt].rotation = 90;
-            tracks[cnt].scale = [0.1, 0.1, 0.1];
-            z += 120;
-            cnt = cnt + 1;
-          }
-          z = -10000;
-          for(var i = 0; i < 200; ++i){
-            tracks.push(initBuffersModel(gl, 0, 0, 0, -58, -50, z, [1.0, 1.0, 0.0, 1.0], modelTrack));
-            tracks[cnt].rotation = 90;
-            tracks[cnt].scale = [0.1, 0.1, 0.1];
-            z += 120;
-            cnt = cnt + 1;
-          }
-          /****************************************************************/
-          /**************************Creating player***********************/
-          player = initBuffersModel(gl, 1, 1, 1, 0, 1.0, 15, [0.0, 0.0, 1.0, 1.0], modelPlayer);
-          player.y_speed = 0.0;
-          player.rotation = 180.0;
-          player.scale = [0.35, 0.35, 0.35];
-          requestAnimationFrame(render);
-          /****************************************************************/
-          /***************************Creating Inspector*******************/
-          inspector = initBuffersModel(gl, 1, 1, 1, 0, 1.0, 50, [0.0, 0.0, 1.0, 1.0], modelInspector);
-          inspector.scale = [0.4, 0.4, 0.4];
-          inspector.rotation = 180;
-          /*****************************************************************/
+          loadJSONResource('./coin.json', function(modelErr5, modelCoin){
+            textureCoin = loadTexture(gl, 'coin.png');
+            textureTrain = loadTexture(gl, 'train.png');
+            textureTrack = loadTexture(gl, 'tracks.png');
+            texturePlayer = loadTexture(gl, 'player.png');
+            textureInspector = loadTexture(gl, 'inspector.png');
+            /* Generating trains */
+            trains.push(initBuffersModel(gl, 1, 1, 1, 0, 2, -150, [1.0, 1.0, 0.0, 1.0],modelTrain));
+            trains[0].rotation = 180;
+            trains[0].scale = [0.25, 0.25, 0.25];
+            /************************************************************/
+            /* Generating tracks */
+            var z = -10000;
+            var cnt = 0;
+            for(var i = 0; i < 200; ++i){
+              tracks.push(initBuffersModel(gl, 0, 0, 0, 0, -50, z, [1.0, 1.0, 0.0, 1.0], modelTrack));
+              tracks[cnt].rotation = 90;
+              tracks[cnt].scale = [0.1, 0.1, 0.1];
+              z += 120;
+              cnt = cnt + 1;
+            }
+            z = -10000;
+            for(var i = 0; i < 200; ++i){
+              tracks.push(initBuffersModel(gl, 0, 0, 0, 58, -50, z, [1.0, 1.0, 0.0, 1.0], modelTrack));
+              tracks[cnt].rotation = 90;
+              tracks[cnt].scale = [0.1, 0.1, 0.1];
+              z += 120;
+              cnt = cnt + 1;
+            }
+            z = -10000;
+            for(var i = 0; i < 200; ++i){
+              tracks.push(initBuffersModel(gl, 0, 0, 0, -58, -50, z, [1.0, 1.0, 0.0, 1.0], modelTrack));
+              tracks[cnt].rotation = 90;
+              tracks[cnt].scale = [0.1, 0.1, 0.1];
+              z += 120;
+              cnt = cnt + 1;
+            }
+            /****************************************************************/
+            /**************************Creating player***********************/
+            player = initBuffersModel(gl, 1, 1, 1, 0, 1.0, 15, [0.0, 0.0, 1.0, 1.0], modelPlayer);
+            player.y_speed = 0.0;
+            player.rotation = 180.0;
+            player.scale = [0.35, 0.35, 0.35];
+            requestAnimationFrame(render);
+            /****************************************************************/
+            /***************************Creating Inspector*******************/
+            inspector = initBuffersModel(gl, 1, 1, 1, 0, 1.0, 40, [0.0, 0.0, 1.0, 1.0], modelInspector);
+            inspector.scale = [0.4, 0.4, 0.4];
+            inspector.rotation = 180;
+            /*****************************************************************/
+            /****************** Creating Coins *********************************/
+            var lane0 = -1.15;
+            var lane1 = 0.0;
+            var lane2 = 1.15;
+            coins.push(initBuffersModel(gl, 1, 1, 1, lane1, 0.4, 1.0, [0.0, 0.0, 10.0, 1.0], modelCoin));
+            coins[0].scale = [5, 5, 5];
+            coins.push(initBuffersModel(gl, 1, 1, 1, lane2, 0.4, 1.0, [0.0, 0.0, 10.0, 1.0], modelCoin));
+            coins[1].scale = [5, 5, 5];
+            coins.push(initBuffersModel(gl, 1, 1, 1, lane0, 0.4, 1.0, [0.0, 0.0, 10.0, 1.0], modelCoin));
+            coins[2].scale = [5, 5, 5];
+            /******************************************************************/
+          })
         })
-        
       })
     })
   });
@@ -155,6 +171,14 @@ function main() {
           player.y_speed = 0.7;
         }
       }
+      else if(e.keyCode == 38){
+        // Up
+        player.location[2] -= 0.1;
+      }
+      else if(e.keyCode == 40){
+        //Down
+        player.location[2] += 0.1;
+      }
     }
     /************************************************************/
     /***********************  tick  *****************************/
@@ -164,7 +188,19 @@ function main() {
       player.y_speed -= 0.03;
     else
       player.y_speed = 0.0;
-    //player.y_speed = max(player.y_speed, 0);
+    for(var i = 0; i < coins.length; ++i){
+      var player_pos_z = player.location[2] * player.scale[2];
+      var coin_pos_z = (coins[i].location[2] * coins[i].scale[2]);
+
+      var player_pos_x = player.location[0] * player.scale[0];
+      var coin_pos_x = (coins[i].location[0] * coins[i].scale[0]);
+      if(Math.abs(coin_pos_z - player_pos_z) <= 0.05 && Math.abs(coin_pos_x - player_pos_x) <= 0.1)
+      {
+        coins.splice(i, 1);
+        document.getElementById('coin_sound').play();
+        i = i - 1;
+      }
+    }
     /************************************************************/
     /************************** Camera **************************/
     const fieldOfView = 45 * Math.PI / 180;   // in radians
@@ -172,8 +208,8 @@ function main() {
     const zNear = 0.1;
     const zFar = 100.0;
     const projectionMatrix = mat4.create();
-    var eye = [0, 10, 34];
-    var look = [0, 0, 0];
+    var eye = [0, 8, (player.location[2] * player.scale[2]) + 20];
+    var look = [0, 0, player.location[2] * player.scale[2]];
     var up = [0, 1, 0];
     // note: glmatrix.js always has the first argument
     // as the destination to receive the result.
@@ -207,12 +243,13 @@ function main() {
       drawObjectTextured(gl, programInfoTextured, tracks[i], deltaTime, viewProjectionMatrix, textureTrack);
     }
     drawObjectTextured(gl, programInfoTextured, player, deltaTime, viewProjectionMatrix, texturePlayer);
-    drawObjectTextured(gl, programInfoTextured, inspector, deltaTime, viewProjectionMatrix, textureInspector);
+    //drawObjectTextured(gl, programInfoTextured, inspector, deltaTime, viewProjectionMatrix, textureInspector);
     // Drawing the trains
     for(var i = 0; i < trains.length; ++i)
       drawObjectTextured(gl, programInfoTextured, trains[i], deltaTime, viewProjectionMatrix, textureTrain);
-    
-      requestAnimationFrame(render);
+    for(var i = 0; i < coins.length; ++i)
+      drawObjectTextured(gl, programInfoTextured, coins[i], deltaTime, viewProjectionMatrix, textureCoin);
+    requestAnimationFrame(render);
     /*****************************************************************/
   }
   //requestAnimationFrame(render);
